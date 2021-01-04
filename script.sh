bb05267c61c905c2c062deebb91e2f8625d2cd28
Andres Tools <andres@caylent.com>
2021-01-04 18:18:30 -0300

    added a step to retrieve private worker IP

--- script.sh
+++ script.sh
@@ -3,5 +3,9 @@
 echo "This is the external IP for this worker"
 curl http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip -H "Metadata-Flavor: Google"
 
+
+echo "This is the private IP for this worker"
+curl http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip -H "Metadata-Flavor: Google"
+
 echo "This is the default service account for this worker"
 curl http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/ -H "Metadata-Flavor: Google"
