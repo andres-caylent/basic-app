bc41ff0fa5a3732bebbc1b73c239e37e0990f253
Andres Tools <andres@caylent.com>
2021-01-12 10:31:44 -0300

    Cleaning cloudbuild.yaml and script.sh files

--- script.sh
+++ script.sh
@@ -1,26 +1,17 @@
 #! bin/bash
 
-#echo "This is the external IP for this worker"
+## Reading external IP from instance metadata. 
 #curl http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip -H "Metadata-Flavor: Google"
 
-
-#echo "This is the private IP for this worker"
-#curl http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip -H "Metadata-Flavor: Google"
-
+## Reading extenal IP from 
 echo "Result from ifconfig.io"
 PUBLIC_IP=$(curl ifconfig.io)
 echo $PUBLIC_IP
 
-### These are needed if you need to read the value from another builder.
+## Sending the IP to file to be able to read it from another builder.
 echo $PUBLIC_IP > /workspace/IP.json
-#echo "reading (cat) workspace/IP.json"
-#cat /workspace/IP.json
 
-echo "This is the default service account for this worker"
-curl http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/ -H "Metadata-Flavor: Google"
-
-
-gcloud container clusters describe gke-e2e-demo --region us-west1-a --impersonate-service-account=gke-custom@andres-testing.iam.gserviceaccount.com
+#gcloud container clusters describe gke-e2e-demo --region us-west1-a --impersonate-service-account=gke-custom@andres-testing.iam.gserviceaccount.com
 
 gcloud container clusters update gke-e2e-demo --enable-master-authorized-networks --master-authorized-networks $PUBLIC_IP/32 --region us-west1-a --impersonate-service-account=gke-custom@andres-testing.iam.gserviceaccount.com
 
