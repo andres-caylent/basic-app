d01707d89511d12906d39ba0836f16d61bd1a55d
Andres Tools <andres@caylent.com>
2021-01-12 12:21:06 -0300

    testing FORMERWHITELIST

--- script.sh
+++ script.sh
@@ -13,6 +13,7 @@ echo $PUBLIC_IP > /workspace/IP.json
 
 ## Getting the former whitelist from cluster.
 FORMERWHITELIST=$(gcloud container clusters describe gke-e2e-demo --region us-west1-a --impersonate-service-account=gke-custom@andres-testing.iam.gserviceaccount.com --format json | jq -r '.masterAuthorizedNetworksConfig.cidrBlocks[].cidrBlock')
+echo $FORMERWHITELIST
 echo $FORMERWHITELIST > /workspace/FORMERWHITELIST.json
 
 gcloud container clusters update gke-e2e-demo --enable-master-authorized-networks --master-authorized-networks $PUBLIC_IP/32 --region us-west1-a --impersonate-service-account=gke-custom@andres-testing.iam.gserviceaccount.com
