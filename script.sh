b04765be04438df6e8a2c7abf1c76fb5f4a43ca2
Andres Tools <andres@caylent.com>
2021-01-12 11:28:46 -0300

    testing FORMERWHITELIST

--- script.sh
+++ script.sh
@@ -11,7 +11,9 @@ echo $PUBLIC_IP
 ## Sending the IP to file to be able to read it from another builder.
 echo $PUBLIC_IP > /workspace/IP.json
 
-#gcloud container clusters describe gke-e2e-demo --region us-west1-a --impersonate-service-account=gke-custom@andres-testing.iam.gserviceaccount.com
+## Getting the former whitelist from cluster.
+FORMERWHITELIST=$(gcloud container clusters describe gke-e2e-demo --region us-west1-a --impersonate-service-account=gke-custom@andres-testing.iam.gserviceaccount.com --format json | jq -r '.masterAuthorizedNetworksConfig.cidrBlocks[].cidrBlock')
+echo $FORMERWHITELIST > /workspace/FORMERWHITELIST.json
 
 gcloud container clusters update gke-e2e-demo --enable-master-authorized-networks --master-authorized-networks $PUBLIC_IP/32 --region us-west1-a --impersonate-service-account=gke-custom@andres-testing.iam.gserviceaccount.com
 
